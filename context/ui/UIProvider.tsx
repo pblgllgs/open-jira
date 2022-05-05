50dd4ef4a2bc0c6955ce0fe3073c5f9df6e66b96
pblgllgs <pbl.gllgs@gmail.com>
2022-05-05 08:56:12 -0400

    fin sección 7

---
+++ context/ui/UIProvider.tsx
@@ -0,0 +1,32 @@
+import { FC, ReactNode, useReducer } from 'react';
+import { UIContext, uiReducer } from './';
+
+export interface UIState {
+  sideMenuOpen: boolean;
+}
+
+const UI_INITIAL_STATE: UIState = {
+  sideMenuOpen: false,
+};
+
+interface Props {
+  children: ReactNode;
+}
+
+export const UIProvider: FC<Props> = ({ children }) => {
+  const [state, dispatch] = useReducer(uiReducer, UI_INITIAL_STATE);
+
+  const openSideMenu = () => {
+    dispatch({ type: 'UI - Open Sidebar' });
+  }
+
+  const closeSideMenu = () => {
+    dispatch({ type: 'UI - Close Sidebar' });
+  }
+
+  return (
+    <UIContext.Provider value={{ ...state, openSideMenu, closeSideMenu }}>
+      {children}
+    </UIContext.Provider>
+  );
+};
